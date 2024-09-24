744861881d7cb2bca2fa01fa4defc4a736ab33a2
David Lozano <davidloz94.dls@gmail.com>
2024-09-24 17:24:02 +0200

    Añadido la funcionalidad de hacer clic a la película y que te muestre su información

--- app/src/main/java/edu/iesam/dam2024/features/movies/data/remote/MovieMockRemoteDataSource.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/data/remote/MovieMockRemoteDataSource.kt
@@ -4,13 +4,15 @@ import edu.iesam.dam2024.features.movies.domain.Movie
 
 class MovieMockRemoteDataSource {
 
+    private val movies = listOf(
+        Movie("1", "title1", "poster1"),
+        Movie("2", "title2", "poster2"),
+        Movie("3", "title3", "poster3"),
+        Movie(title = "title4", poster = "poster4", id = "4")
+    )
+
     fun getMovies(): List<Movie> {
-        return listOf(
-            Movie("1", "title1", "poster1"),
-            Movie("2", "title2", "poster2"),
-            Movie("3", "title3", "poster3"),
-            Movie(title = "title4", poster = "poster4", id = "4")
-        )
+        return movies
     }
 
     fun getMovieById(movieId: String): Movie? {
--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieActivity.kt
@@ -2,40 +2,61 @@ package edu.iesam.dam2024.features.movies.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import edu.iesam.dam2024.R
+import edu.iesam.dam2024.features.movies.domain.Movie
 
 class MovieActivity : AppCompatActivity() {
 
     private val movieFactory: MovieFactory = MovieFactory()
+    private val viewModel = movieFactory.buildViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val viewModel = movieFactory.buildViewModel()
+
         val movies = viewModel.viewCreated()
-        Log.d("@dev", movies.toString())
-        val movie = viewModel.itemSelected(movies.first().id)
-        Log.d("@dev", movie.toString())
+        binData(movies)
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("@dev", "@onResume")
-    }
+    private fun binData(movies: List<Movie>) {
+        findViewById<TextView>(R.id.movie_id_1).text = movies[0].id
+        findViewById<TextView>(R.id.movie_title_1).text = movies[0].title
+        findViewById<LinearLayout>(R.id.layout_1).setOnClickListener {
+            val movie1: Movie? = viewModel.itemSelected(movies[0].id)
+            movie1?.let {
+                Log.d("@dev", "Pelicula seleccionada: ${it.title}")
+            }
+        }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d("@dev", "@onPause")
-    }
+        findViewById<TextView>(R.id.movie_id_2).text = movies[1].id
+        findViewById<TextView>(R.id.movie_title_2).text = movies[1].title
+        findViewById<LinearLayout>(R.id.layout_2).setOnClickListener {
+            val movie2: Movie? = viewModel.itemSelected(movies[1].id)
+            movie2?.let {
+                Log.d("@dev", "Pelicula seleccionada: ${it.title}")
+            }
+        }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("@dev", "@onStart")
-    }
+        findViewById<TextView>(R.id.movie_id_3).text = movies[2].id
+        findViewById<TextView>(R.id.movie_title_3).text = movies[2].title
+        findViewById<LinearLayout>(R.id.layout_3).setOnClickListener {
+            val movie3: Movie? = viewModel.itemSelected(movies[2].id)
+            movie3?.let {
+                Log.d("@dev", "Pelicula seleccionada: ${it.title}")
+            }
+        }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("@dev", "@onDestroy")
+        findViewById<TextView>(R.id.movie_id_4).text = movies[3].id
+        findViewById<TextView>(R.id.movie_title_4).text = movies[3].title
+        findViewById<LinearLayout>(R.id.layout_4).setOnClickListener {
+            val movie4: Movie? = viewModel.itemSelected(movies[3].id)
+            movie4?.let {
+                Log.d("@dev", "Pelicula seleccionada: ${it.title}")
+            }
+        }
     }
 }
