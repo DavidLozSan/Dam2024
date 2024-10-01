2dfea0237195acbd376c8ea82fbb54a832960aaa
David Lozano <davidloz94.dls@gmail.com>
2024-10-01 16:32:31 +0200

    Cambio el nombre a plural del main movies y superheores

--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MoviesActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MoviesActivity.kt
@@ -9,14 +9,14 @@ import edu.iesam.dam2024.R
 import edu.iesam.dam2024.features.movies.data.local.MovieXmlLocalDataSource
 import edu.iesam.dam2024.features.movies.domain.Movie
 
-class MovieActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
 
     private lateinit var movieFactory: MovieFactory
     private lateinit var viewModel: MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
 
         movieFactory = MovieFactory(this)
         viewModel = movieFactory.buildViewModel()
