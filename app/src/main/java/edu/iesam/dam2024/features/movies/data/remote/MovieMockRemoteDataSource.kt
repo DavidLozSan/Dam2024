f8e4d288ad3e67d1613e93d9ba8fb37b954592ef
David Lozano <davidloz94.dls@gmail.com>
2024-10-01 19:04:18 +0200

    Añadido MovieDetails

--- app/src/main/java/edu/iesam/dam2024/features/movies/data/remote/MovieMockRemoteDataSource.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/data/remote/MovieMockRemoteDataSource.kt
@@ -7,7 +7,7 @@ class MovieMockRemoteDataSource {
     private val movies = listOf(
         Movie("1", "title1", "poster1"),
         Movie("2", "title2", "poster2"),
-        Movie("3", "title3", "poster3"),
+        Movie("3", "title3", "https://www.soydemadrid.com/images/thumbs/origenes-secretosla-nueva-pelicula-de-netflix-con--0051968.jpeg"),
         Movie(title = "title4", poster = "poster4", id = "4")
     )
 
