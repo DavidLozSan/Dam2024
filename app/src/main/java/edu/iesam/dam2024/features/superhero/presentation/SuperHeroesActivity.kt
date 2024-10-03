7350dbf9e30d0ad47786970a10a388d7d3d10898
DAVILOZ <davidloz94.dls@gmail.com>
2024-10-03 19:32:27 +0200

    Añadido el listado de superheroes. Solucion de problemas

--- app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroesActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/superhero/presentation/SuperHeroesActivity.kt
@@ -30,6 +30,24 @@ class SuperHeroActivity : AppCompatActivity() {
         findViewById<LinearLayout>(R.id.layout_1).setOnClickListener {
             navigateToSuperheroDetail(superheroes[0].principalData.id)
         }
+
+        findViewById<TextView>(R.id.superhero_id_2).text = superheroes[1].principalData.id
+        findViewById<TextView>(R.id.superhero_name_2).text = superheroes[1].principalData.name
+        findViewById<LinearLayout>(R.id.layout_2).setOnClickListener {
+            navigateToSuperheroDetail(superheroes[1].principalData.id)
+        }
+
+        findViewById<TextView>(R.id.superhero_id_3).text = superheroes[2].principalData.id
+        findViewById<TextView>(R.id.superhero_name_3).text = superheroes[2].principalData.name
+        findViewById<LinearLayout>(R.id.layout_3).setOnClickListener {
+            navigateToSuperheroDetail(superheroes[2].principalData.id)
+        }
+
+        findViewById<TextView>(R.id.superhero_id_4).text = superheroes[3].principalData.id
+        findViewById<TextView>(R.id.superhero_name_4).text = superheroes[3].principalData.name
+        findViewById<LinearLayout>(R.id.layout_4).setOnClickListener {
+            navigateToSuperheroDetail(superheroes[3].principalData.id)
+        }
     }
 
     private fun navigateToSuperheroDetail(superheroId: String) {
